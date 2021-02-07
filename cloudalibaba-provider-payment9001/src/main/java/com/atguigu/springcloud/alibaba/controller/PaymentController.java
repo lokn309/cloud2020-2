f91dc7e46accba41bf9e33f58793d8bd62fb47d6
lokn <lokn329@aliyun.com>
2021-02-07 23:43:56 +0800

    add spring cloud alibaba

---
+++ cloudalibaba-provider-payment9001/src/main/java/com/atguigu/springcloud/alibaba/controller/PaymentController.java
@@ -0,0 +1,27 @@
+package com.atguigu.springcloud.alibaba.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+/**
+ * @description:
+ * @author: lokn
+ * @date: 2021/02/07 20:53
+ */
+@RestController
+public class PaymentController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping("/payment/nacos/{id}")
+    public String getPayment(@PathVariable("id") Integer id) {
+        return "nacos registry, serverPort: " + serverPort + "\t id=" + id;
+    }
+
+
+}
